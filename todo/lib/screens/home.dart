0f78717d7ddbbf7f099ad0d4bafe76a31c656980
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2023-03-16 17:33:03 -0700

    Created Title

--- todo/lib/screens/home.dart
+++ todo/lib/screens/home.dart
@@ -14,6 +14,22 @@ class Home extends StatelessWidget {
         child: Column(
           children: [
             searchBox(),
+            Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 50, bottom: 20),
+                    child: Text(
+                      'All ToDos',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
